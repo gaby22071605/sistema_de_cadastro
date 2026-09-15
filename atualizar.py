6e8026ad700724469038616a3e23c25160f973e2
gaby22071605 <gabycristina2207@gmail.com>
2026-09-14 21:51:08 -0300

    Adicionar função para atualizar cliente no banco de dados
    Implementa a função para atualizar informações do cliente, incluindo validação de campos obrigatórios.

---
+++ atualizar.py
@@ -0,0 +1,32 @@
+
+def atualizar_cliente(banco_dados):
+
+    if len(banco_dados) == 0:
+        print('Nenhum cliente cadastrado.')
+        return None
+
+    id_cliente = int(input('Digite o ID do cliente: '))
+
+    cliente_atualizado = None
+    for cliente in banco_dados:
+        if cliente['id'] == id_cliente:
+            cliente_atualizado = cliente
+            break
+
+    nome = input('Nome: ').strip()
+    idade = input('Idade: ').strip()
+    telefone = input('Telefone: ').strip()
+    email = input('Email: ').strip()
+
+    if nome == '' or idade == '' or telefone == '' or email == '':
+        print('Erro: Todos os campos são obrigatorios')
+        return None
+    cliente_atualizado['nome'] = nome
+    cliente_atualizado['idade'] = idade
+    cliente_atualizado['telefone'] = telefone
+    cliente_atualizado['email'] = email
+
+    print('Cliente atualizado com sucesso!')
+
+
+    
